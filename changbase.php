e5c384766818912c9b7e1fd573a8d370255badcd
doraLiang <373695879@qq.com>
2018-03-01 12:00:03 +0800

    Update changbase.php

--- changbase.php
+++ changbase.php
@@ -1,4 +1,9 @@
 <?php
-$oMyNum = $_GET['text'];
-echo $oMyNum;
+header("Content-type:text/html;charset=utf-8");
+    $username = $_POST['username'];
+    $age = $_POST['age'];
+    $job = $_POST['job'];
+    $json_arr = array("username"=>$username,"age"=>$age,"job"=>$job);
+    $json_obj = json_encode($json_arr);
+    echo $json_obj;
 ?>
